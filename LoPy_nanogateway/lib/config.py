e93feace73de948bdcfba69dabd3bd1e2390c17d
abeljimenez@outlook.com <ab>
2020-05-25 00:41:41 +0200

    add nanoGateway

---
+++ LoPy_nanogateway/lib/config.py
@@ -0,0 +1,25 @@
+import machine
+import ubinascii
+
+WIFI_MAC = ubinascii.hexlify(machine.unique_id()).upper()
+# Set  the Gateway ID to be the first 3 bytes of MAC address + 'FFFE' + last 3 bytes of MAC address
+GATEWAY_ID = WIFI_MAC[:6] + "ABCD" + WIFI_MAC[6:12]
+
+SERVER = 'router.eu.thethings.network'
+PORT = 1700
+
+NTP = "pool.ntp.org"
+NTP_PERIOD_S = 3600
+
+WIFI_SSID = 'ADREL_86_79'
+WIFI_PASS = 'Gustavo_Granollers79@Aianeta_EIXAMPLE86'
+
+# for EU868
+LORA_FREQUENCY = 868100000
+LORA_GW_DR = "SF7BW125" # DR_5
+LORA_NODE_DR = 5
+
+# for US915
+# LORA_FREQUENCY = 903900000
+# LORA_GW_DR = "SF7BW125" # DR_3
+# LORA_NODE_DR = 3
